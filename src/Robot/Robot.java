2cb2c4ad7525c59eb8c7eaca7b3e0e905b04003f
Dllite <dilanzambou2@gmail.com>
2022-12-01 11:40:21 +0100

    Progression avec la classe Arena

--- src/Robot/Robot.java
+++ src/Robot/Robot.java
@@ -24,15 +24,10 @@ public class Robot {
 			robotAttaque.nbVie -=2;
 			System.out.println(robotAttaque.nom + " a ete touche par le robot " + this.nom);
 			System.out.println("Au " + robotAttaque.nom + " reste " + robotAttaque.nbVie);
-			
 		}
-		
-	
-		
-		
 		//robotAttaque.setNbVie(robotAttaquenbVie()- 2); 
 		//System.out.println(robotAttaque.getNom() + " a ete touche par le robot " + this.nom); 
-		//System.out.println("Au  " + robotAttaque.getNomRobot()  + " il reste " + robotAttaque.getNbreVie()); 
+		//System .out.println("Au  " + robotAttaque.getNomRobot()  + " il reste " + robotAttaque.getNbreVie()); 
 		//this.nbVie-=R.nbVie;
 		//System.out.println(this.nom + " a recu un coup de " + R.nom);
 		//System.out.println(this.nom + " Niveau de vie : " + (R.nbVie-2));
@@ -40,17 +35,19 @@ public class Robot {
 		//System.out.println(this.nbVie);
 		//System.out.println(this.nom + " est envie ? :  " +isDead(this.nbVie));
 		//System.out.println(R.nom + " est envie ? :  " +isDead(R.nbVie));
+		 
 		
 
 	}
-	
 	public Boolean isDead(int a) {
-		//int a <= 0;
-		//public Boolean a;
 		if(a<=0) {
+			return true;
+		}else {
 			return false;
-		}return true;
+		}
 	}
+	
+	
 	public void setNbVie(int vie) {
 		this.nbVie=vie;
 	}
@@ -68,6 +65,10 @@ public class Robot {
         Robot bob = new Robot("Bob");
         Robot bob2 = new Robot();
         
+       bob.fire(bob2);
+       bob.fire(bob2);
+       bob.fire(bob2);
+       bob.fire(bob2);
        bob.fire(bob2);
        bob.fire(bob2);
        
